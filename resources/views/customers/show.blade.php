b0179f8c9efb71c0b538f273c627229d1269ca08
rahulkainera <amohanty@unomaha.edu>
2017-02-25 05:54:11 -0600

    Update show.blade.php
    reset to old

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -41,68 +41,7 @@
 
 
             </tbody>
-      </table>
-    </div>
-
-
-          
-    <h3>
-        <?php echo 'Total of Initial Stock Portfolio $' , number_format ($stotal,2); ?>
-        <br>
-        <?php echo 'Total of Current Stock Portfolio $', number_format ($svalue,2) ?>
-    </h3>
-    </div>
-
-    <br>
-    <h2>Investments </h2>
-    <div class="container">
-        <table class="table table-striped table-bordered table-hover">
-            <thead>
-            <tr class="bg-info">
-                <th> Category </th>
-                <th>Description</th>
-                <th>Acquired Value</th>
-                <th>Acquired Date</th>
-                <th>Recent Value</th>
-                <th>Recent Date</th>
-
-            </tr>
-            </thead>
-
-            <tbody>
-
-
-
-
-            @foreach($customer->investments as $investment)
-                <tr>
-                    <td>{{ $investment->category }}</td>
-                    <td>{{ $investment->description }}</td>
-                    <td>{{ $investment->acquired_value }}</td>
-                    <td>{{ $investment->acquired_date }}</td>
-                    <td>{{ $investment->recent_value }}</td>
-                    <td>{{ $investment->recent_date }}</td>
-                    <?php $itotal = $itotal + $investment['acquired_value']?>
-                    <?php $ivalue = $ivalue + $investment['recent_value'] ?>
-                </tr>
-
-            @endforeach
-
-            </tbody>
         </table>
-        <h3>
-        <?php echo 'Total of Initial Investment Portfolio $', number_format($itotal,2); ?>
-        <br>
-        <?php echo 'Total of Current Investment Portfolio $', number_format($ivalue,2); ?>
-        </h3>
-        <br>
-        <h2>Summary of Portfolio </h2>
-        <h3>
-        <?php $iportfolio = $stotal + $itotal;?>
-        <?php echo 'Total of Initial Portfolio Value $', number_format($iportfolio,2); ?>        <br>
-        <?php $cportfolio = $svalue + $ivalue;?>
-        <?php echo 'Total of Current Investment Portfolio $', number_format($cportfolio,2) ?>
-        </h3>
     </div>
 
 @stop
